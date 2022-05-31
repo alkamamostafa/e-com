0a7676a1f78c0edd7bb42d1377d4369182f2b64e
alkama mustapha <alkama.mostafa@gmail.com>
2022-05-31 22:22:45 +0100

    git commit -m "refactor"

--- src/main/java/com/maa/alk/ecommerce/service/graphql/GraphQLProvider.java
+++ src/main/java/com/maa/alk/ecommerce/service/graphql/GraphQLProvider.java
@@ -27,7 +27,7 @@ public class GraphQLProvider {
     private final OrderService orderService;
     private final UserService userService;
 
-    @Value("classpath:graphql/schemas.graphql")
+    @Value("**/*.graphql")
     private Resource resource;
 
     @Getter
